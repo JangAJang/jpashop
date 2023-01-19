765f7f403dc969bc03e3f6c578660b5998028de0
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-19 20:14:04 +0900

    feat: MemberRepositoryTest 멤버 생성 및 저장을 테스트하는 코드 생성(junit5로 바뀌면서 @RunWith -> @ExtendWith로 변경. @Autowired에서 문제가 있어 수정 예정)

--- src/test/java/com/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/com/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -3,18 +3,22 @@ package com.jpabook.jpashop.repository;
 import com.jpabook.jpashop.entity.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
-    public void testMember(){
+    @Transactional
+    public void testMember() throws Exception{
         //given
         Member member = new Member();
         member.setName("name1");
@@ -26,6 +30,5 @@ class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getName()).isEqualTo("name1");
         Assertions.assertThat(findMember.getId()).isEqualTo(savedId);
-
     }
 }
