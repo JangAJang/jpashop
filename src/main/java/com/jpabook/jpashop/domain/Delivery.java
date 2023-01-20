6f02f2818d55b7c99efb0897fa0fb1a7d86452e4
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-20 17:22:38 +0900

    feat: Delivery 엔티티 생성

---
+++ src/main/java/com/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,22 @@
+package com.jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "DELIVERY_ID")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus;
+}
