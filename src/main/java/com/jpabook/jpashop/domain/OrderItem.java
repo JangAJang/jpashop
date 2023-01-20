89fb41f1cd77a492f832a33964cfbb0d02e833e4
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-20 17:21:47 +0900

    feat: OrderItem 엔티티 생성

---
+++ src/main/java/com/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,29 @@
+package com.jpabook.jpashop.domain;
+
+import com.jpabook.jpashop.domain.item.Item;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    @Column(name = "ORDER_ITEMS_PRICE")
+    private int orderPrice;
+
+    @Column(name = "ORDER_ITEM_COUNT")
+    private int count;
+}
