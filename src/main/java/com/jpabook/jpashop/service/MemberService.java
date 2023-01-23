058ab78d98e51bddd92ecdc780885d8c2f120272
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-23 15:53:56 +0900

    feat: MemberService 멤버 비즈니스 로직을 수행하는 서비스 추가

---
+++ src/main/java/com/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,35 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberService {
+
+    @Autowired
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member){
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member){
+        if(!memberRepository.findByName(member.getName()).isEmpty()){
+            throw new IllegalArgumentException("이미 가입된 회원입니다");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Member findOne(Long id){
+        return memberRepository.findOneById(id);
+    }
+
+}
