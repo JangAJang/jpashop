4d2f07a58efd6fe52f828fffa3129a7c289c4a5d
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-23 17:54:06 +0900

    feat: Order 주문을 일괄적으로 생성해주는 기능 구현

--- src/main/java/com/jpabook/jpashop/domain/Order.java
+++ src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -1,11 +1,15 @@
 package com.jpabook.jpashop.domain;
 
+import com.jpabook.jpashop.exception.delivery.DeliveryAlreadyCompleteException;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static com.jpabook.jpashop.domain.OrderStatus.CANCEL;
 
 @Entity
 @Table(name = "ORDERS")
@@ -32,4 +36,15 @@ public class Order {
 
     @Enumerated(value = EnumType.STRING)
     private OrderStatus orderStatus;
+
+    // 생성 메서드, 주문 생성에 변경점이 있으면 이 부분만 바꾸면 된다.
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        order.orderItems.addAll(Arrays.asList(orderItems));
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
 }
