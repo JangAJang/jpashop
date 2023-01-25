72b4d4ecda0640b2ed5cede9e431d45dc5d4460d
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-25 14:13:37 +0900

    feat: MemberController 회원가입창으로 이동시키는 컨트롤러 기능 구현

---
+++ src/main/java/com/jpabook/jpashop/dto/JoinMemberDto.java
@@ -0,0 +1,17 @@
+package com.jpabook.jpashop.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class JoinMemberDto {
+
+    @NotEmpty(message = "회원 이름을 입력해야합니다.")
+    private String name;
+
+    private String city;
+
+    private String street;
+
+    private String zipcode;
+}
