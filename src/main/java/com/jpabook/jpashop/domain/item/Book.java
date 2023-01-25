0c5fc1d8d74422fe2b07da2f7a7426893e83c61a
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-25 14:39:37 +0900

    feat: Book View에서 사용하는 BookForm을 이용한 Book 생성 메서드 구현

--- src/main/java/com/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/com/jpabook/jpashop/domain/item/Book.java
@@ -1,5 +1,6 @@
 package com.jpabook.jpashop.domain.item;
 
+import com.jpabook.jpashop.dto.BookForm;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Data;
@@ -13,4 +14,15 @@ public class Book  extends Item{
 
     @Column(name = "BOOK_ISBN")
     private String isbn;
+
+    public static Book createBook(BookForm form){
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+        book.setId(form.getId());
+        return book;
+    }
 }
