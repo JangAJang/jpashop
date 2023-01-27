e392a55323ac725cf981d017adf9e566c289d8e1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-27 20:11:50 +0900

    feat: Hibernate5Module 연관관계의 엔티티를 프록시로 불러올 때 캐시에 없어도 강제로 가져오는 기능 구현

--- src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -23,6 +23,13 @@ public class OrderSimpleApiController {
         이렇게 무한루프에 빠지게 된다.
         Order와 양방향으로 관계를 갖는 엔티티에 JsonIgnore를 추가해주어야 한다.
          */
-
+        /*
+        ~~ToOne으로 매핑된 엔티티들을 모두 프록시 객체를 생성해서 연관관계의 엔티티를 가져온다.
+        ByteBuddyInterceptor로 대신해서 나오게 된다.(= 프록시로 초기화해서 나온다)
+        Hibernate5Module를 이용해 호출하면 이렇게 프록시로 불러와야 하는데 그러지 못하는 엔티티들을 null로 처리해버리고
+        호출한 엔티티를 반환해준다.
+        반대로, configure에서 강제로 로딩시키면 가져올 수 있다.
+         */
+        return orderRepository.findAllByString(new OrderSearch());
     }
 }
