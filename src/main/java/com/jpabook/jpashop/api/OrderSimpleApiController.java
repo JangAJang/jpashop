37a000a5f78623af9ce9b54b64bfda7027bdc8da
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-27 20:22:48 +0900

    feat: OrderSimpleApiController 엔티티를 dto로 바꿔 조회하는 기능 구현. 프록시로 인한 문제는 없지만 N+1문제가 여전ㄴ히 존재

--- src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,6 +1,8 @@
 package com.jpabook.jpashop.api;
 
 import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.dto.ResultDto;
+import com.jpabook.jpashop.dto.order.SimpleOrderDto;
 import com.jpabook.jpashop.repository.OrderRepository;
 import com.jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,7 +32,23 @@ public class OrderSimpleApiController {
         Hibernate5Module를 이용해 호출하면 이렇게 프록시로 불러와야 하는데 그러지 못하는 엔티티들을 null로 처리해버리고
         호출한 엔티티를 반환해준다.
         반대로, configure에서 강제로 로딩시키면 가져올 수 있다.
+
+        제일 중요한 것은, 엔티티 자체가 외부로 나가면 안된다.
+        보안상 안전하게, 필요한 데이터만 모아서 dto로 반환시켜주는것이 안전하다.
          */
         return orderRepository.findAllByString(new OrderSearch());
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public ResultDto ordersV2(){
+        /*
+        이러한 방식도 Lazy로딩으로 인한 과도한 쿼리 발생 문제가 있다.
+        N+1 문제
+        1. Order N개를 찾는 쿼리 1개
+        2. Order 1개당 하나씩 있는 Member를 하나씩 찾는 쿼리 N개
+        3. Order 1개당 하나씩 있는 Delivery를 하나씩 찾는 쿼리 N개
+         */
+        return new ResultDto(orderRepository.findAllByString(new OrderSearch()).stream().map(SimpleOrderDto::new)
+                .collect(Collectors.toList()));
     }
 }
---
+++ src/main/java/com/jpabook/jpashop/dto/order/SimpleOrderDto.java
@@ -0,0 +1,26 @@
+package com.jpabook.jpashop.dto.order;
+
+import com.jpabook.jpashop.domain.Address;
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class SimpleOrderDto {
+
+    private Long id;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public SimpleOrderDto(Order order){
+        this.id = order.getId();
+        name = order.getMember().getName();
+        orderDate = order.getOrderDate();
+        orderStatus = order.getOrderStatus();
+        address = order.getDelivery().getAddress();
+    }
+}
